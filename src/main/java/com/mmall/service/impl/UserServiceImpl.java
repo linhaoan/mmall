ef24d3608413d80c8b297a9b0c4d14e9f282828b
linhao <330966654@qq.com>
2018-05-15 15:02:09 +0800

    用户管理

--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -14,7 +14,7 @@ import org.springframework.util.StringUtils;
 import java.util.UUID;
 
 /**
- * Created by geely
+ * Created by linhao
  */
 @Service("iUserService")
 public class UserServiceImpl implements IUserService {
